f9eaab00e6d06eae6273d365a02eec9d2ffa0ddc
Kate Anderson <kate.anderson250@gmail.com>
2022-04-25 16:23:14 -0700

    Potato boy

--- config.js
+++
@@ -1,8 +0,0 @@
-const PORT = 3000;
-const TOKEN = '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';
-const USER = 'kateanderson';
-const HOST = 'localhost';
-const DB = 'sdc';
-const DBPORT = 5432;
-
-module.exports = { PORT, TOKEN, USER, HOST, DB, DBPORT };
