b6710c3ef199a278f3a9800a7ddd17127868ec99
Kate Anderson <kate.anderson250@gmail.com>
2022-04-26 18:39:03 -0700

    Removes LOADER url

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,8 +9,5 @@ router.get('/reviews/', controllers.get);
 router.post('/reviews', controllers.post);
 router.put('/reviews/:review_id/helpful', controllers.helpful);
 router.put('/reviews/:review_id/report', controllers.report);
-router.get('/reviews/loaderio-a3e8135f3c9289061c6536ac6cd049fa/', (req, res) => {
-  res.send(LOADER);
-});
 
 module.exports = router;
