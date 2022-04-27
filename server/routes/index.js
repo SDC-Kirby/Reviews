137644efdf1cbaa74220a9dc0688755518c5e0c4
Kate Anderson <kate.anderson250@gmail.com>
2022-04-26 19:57:27 -0700

    Adds loader endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,5 +9,8 @@ router.get('/reviews/', controllers.get);
 router.post('/reviews', controllers.post);
 router.put('/reviews/:review_id/helpful', controllers.helpful);
 router.put('/reviews/:review_id/report', controllers.report);
+router.get('/loaderio-a3e8135f3c9289061c6536ac6cd049fa.txt', (req, res) => {
+  res.send('loaderio-a3e8135f3c9289061c6536ac6cd049fa');
+});
 
 module.exports = router;
