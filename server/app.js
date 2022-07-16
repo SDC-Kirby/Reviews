f17fb857864b66d3ddc89b65b5cef4461e74a2fc
Kate Anderson <kate.anderson250@gmail.com>
2022-07-16 15:42:50 -0700

    Uncomments in app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-app.options("/*", function(req, res){
+app.options('/*', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -15,7 +15,6 @@ app.options("/*", function(req, res){
 });
 
 app.use('/', router);
-
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
